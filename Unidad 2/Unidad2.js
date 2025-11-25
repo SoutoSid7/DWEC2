9472d5ff631fa1cfc08ed0d3579cccdab6b77f89
SoutoSid7 <mariojesussoutocolon@gmail.com>
2025-11-25 09:59:23 +0100

    Unidad 2

---
+++ Unidad 2/Unidad2.js
@@ -0,0 +1,12 @@
+// Variables
+var numero1
+var literal1, litreal2
+var numero3 = 8
+numero1 = 24
+numero1 += 3
+
+// typeof, devuelve el tipo de expresion 
+typeof "abc" // string(primitivo)
+typeof String("abc") // string
+typeof new String("abc") // object
+typeof (new String("abc")).valueOf() // string
