c78c942066cb515a88f32b0b755a809fa9995212
mariosouto <mariosouto226@gmail.com>
2025-12-06 13:26:46 +0100

    Act1Eval2025_Mario_Souto

--- Acti1Eval2025_Mario_Souto/js/cmdb.js
+++ Acti1Eval2025_Mario_Souto/js/cmdb.js
@@ -1,18 +1,41 @@
-export class Genero{
+export class Genero { 
     constructor(id, nombre){
         this.id = id;
         this.nombre = nombre;
     }
 }
 
-export class Peliculas{
-    constructor(id, titulo) {
-        this.id = id;
-        this.titulo = titulo;
+export class Pelicula {
+    constructor(id, titulo, fechaEstreno, popularidad, generos = []){
+        this.id = id
+        this.titulo = titulo
+        this.fechaEstreno = fechaEstreno
+        this.popularidad = popularidad
+        this.puntuaciones = []
+        this.generos = generos
     }
+
+    get puntuacion() {
+        if(this.puntuaciones.length === 0) return 0
+        return Math.round(
+            this.puntuaciones.reduce((a,b)=>a+b,0) / this.puntuaciones.length
+            // Suma todos los valores de this.puntuaciones
+            // Divide entre la cantidad de elementos
+            // Redondea al entero + cercano
+        )
+    }
+
+    get numVotos() {
+        return this.puntuaciones.length
+    }
+
+    votar(valor) {
+        this.puntuaciones.push(valor)
+    }
+
 }
 
-export class Listado{
+export class CMDB {
     // Generos
     static cargarGeneros(){
         return JSON.parse(localStorage.getItem("cmdb_generos") || "[]");
@@ -23,9 +46,20 @@ export class Listado{
 
     // Peliculas
     static cargarPeliculas(){
-        return JSON.parse(localStorage.getItem("cmdb_peliculas") || "[]");
+        let datos = JSON.parse(localStorage.getItem("cmdb_peliculas") || "[]");
+        return datos.map(p => {
+            let peli = new Pelicula(
+                p.id,
+                p.titulo,
+                p.fechaEstreno,
+                p.popularidad,
+                p.generos
+            )
+            peli.puntuaciones = p.puntuaciones || []
+            return peli
+        })
     }
-    static guardarPeliculas(peliculas){
-        localStorage.setItem("cmdb_peliculas", JSON.stringify(peliculas))
+    static guardarPeliculas(lista){
+        localStorage.setItem("cmdb_peliculas", JSON.stringify(lista))
     }
 }
--- Acti1Eval2025_Mario_Souto/js/generos.js
+++ Acti1Eval2025_Mario_Souto/js/generos.js
@@ -1,11 +1,106 @@
 import { Genero, Listado } from "./cmdb.js"
 
 let generos = [];
+let filaSeleccionada = null
 document.addEventListener("DOMContentLoaded", () => {
     generos = Listado.cargarGeneros(); // Lee localStorage
     mostrarTabla();
+
+    document.getElementById("btnAlta").addEventListener("click", altaGenero)
+    document.getElementById("btnBaja").addEventListener("click", bajaGenero)
+    document.getElementById("btnMod").addEventListener("click", modificarGenero)
 })
 
 function mostrarTabla(){
-    
+    const tbody = document.getElementById("tablaGeneros")
+    tbody.innerHTML = ""
+
+    generos.forEach((g) => {
+        const fila = document.createElement("tr")
+
+        fila.innerHTML = `
+        <td>${g.id}</td>
+        <td>${g.nombre}</td>
+        `
+
+        fila.addEventListener("click", () => {
+            filaSeleccionada = g
+            cargarFormulario(g)
+        })
+
+        tbody.appendChild(fila)
+    })
+}
+
+function cargarFormulario(g) {
+    document.getElementById("textId").value = g.id
+    document.getElementById("nombre").value = g.nombre
+}
+
+function conseguirID() {
+    if (generos.length === 0) return 1
+    return generos[generos.length - 1].id + 1
+}
+
+function validarNombre(nombre){
+    if (nombre.trim() === ""){
+        alert("El nombre no puede estar vacio")
+        return false
+    }
+    if (nombre.length > 100){
+        alert("El nombre no puede tener mas de 100 caracteres")
+        return false
+    }
+    return true
+}
+
+function altaGenero() {
+    const nombre = document.getElementById("nombre").value.trim()
+    if(!validarNombre(nombre)) return
+    const nuevoGenero = new Genero(conseguirID(), nombre)
+
+    generos.push(nuevoGenero)
+    Listado.guardarGeneros(generos)
+    mostrarTabla()
+    limpiarFormulario()
+}
+
+function bajaGenero() {
+    const id = parseInt(document.getElementById("textId").value)
+
+    if(!id) {
+        alert("Selecciona un ID")
+        return
+    }
+
+    generos = generos.filter(g => g.id !== id)
+    Listado.guardarGeneros(generos)
+    mostrarTabla()
+    limpiarFormulario()
+}
+
+function modificarGenero() {
+    const id = parseInt(document.getElementById("textId").value)
+    const nombre = document.getElementById("nombre").value.trim()
+
+    if(!id) {
+        alert("Seleccione un ID")
+        return
+    }
+
+    if(!validarNombre(nombre)) return
+    const genero = generos.find(g => g.id === id)
+    if(!genero) return
+
+    genero.nombre = nombre
+
+    Listado.guardarGeneros(generos)
+    mostrarTabla()
+    limpiarFormulario()
+}
+
+function limpiarFormulario() {
+    document.getElementById("textId").value = ""
+    document.getElementById("nombre").value = ""
+    filaSeleccionada = null
 }
