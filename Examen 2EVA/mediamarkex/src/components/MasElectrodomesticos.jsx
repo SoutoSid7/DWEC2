a144b76a565b6bda62972fb84f679cf6abea07ad
SoutoSid7 <mariojesussoutocolon@gmail.com>
2026-05-13 12:50:13 +0200

    Examen 2EVA

--- Examen 2EVA/mediamarkex/src/components/MasElectrodomesticos.jsx
+++ Examen 2EVA/mediamarkex/src/components/MasElectrodomesticos.jsx
@@ -2,11 +2,7 @@ const MasElectrodomesticos = ({ electrodomesticos }) => {
     if (electrodomesticos.length === 0){
         return <p>Cargando electrodomestico mas popular</p>
     }
-
-    /**
-     * reduce -> recorre el array y quedarse con un unico resultado
-     * cancion.popularity > max.popularity ? cancion : max -> quedarse con la cancion con mayor popularity
-     */
+    
     const MasElectrodomesticos = electrodomesticos.reduce((max, electrodomestico) => 
         electrodomestico.sum_w > max.sum_w ? electrodomestico : max
     )
